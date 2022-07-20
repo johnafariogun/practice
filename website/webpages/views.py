4bc43b0b1891d7160983d66a386962470b1e980e
johnafariogun <83656470+johnafariogun@users.noreply.github.com>
2022-07-20 17:19:42 +0100

    updating some comments into views.py

--- website/webpages/views.py
+++ website/webpages/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render, redirect
-from .models import job
-from .forms import ListingForms
-# Create your views here.
+from .models import job #importing models
+from .forms import ListingForms # importing forms
+# Create your views here. explaing functions based view
 def index(request):
-    return render(request, 'webpages/index.html')
+    return render(request, 'webpages/index.html') #home page view
 
 def list_jobs(request):
     listing = job.objects.all()
     context = {
         'listing': listing
     }
-    return render(request, 'webpages/jobs.html', context)
+    return render(request, 'webpages/jobs.html', context) # listing view
 
-def retrieve_jobs(request, pk):
+def retrieve_jobs(request, pk): # retrieve view
     retrieve = job.objects.get(id=pk)
     context = {
         'retrieve': retrieve
     }
     return render(request, 'webpages/ret.html', context)
 
-def listing_create(request):
+def listing_create(request): # create view
     form = ListingForms()
     if request.method == 'POST':
         form =ListingForms(request.POST)
@@ -32,7 +32,7 @@ def listing_create(request):
     return render(request, 'webpages/create.html', context)
 
 
-def listing_update(request, pk):
+def listing_update(request, pk): # update view
     updates = job.objects.get(id=pk)
     form = ListingForms(instance = updates)
 
@@ -47,7 +47,7 @@ def listing_update(request, pk):
     }
     return render(request, 'webpages/update.html', context)
 
-def listing_delete(request, pk):
+def listing_delete(request, pk): # list view
     listing = job.objects.get(id=pk)
     listing.delete()
     return redirect('/jobs')
